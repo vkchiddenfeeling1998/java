9429b35b2ef8ad07ced31600e45a89b16cac89c6
vkchiddenfeeling1998 <67635161+vkchiddenfeeling1998@users.noreply.github.com>
2020-09-19 00:58:20 +0530

    Update Q23reversearray.java

--- Assignment1/Q23reversearray.java
+++ Assignment1/Q23reversearray.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
-public class Q24search{
-	public static void main(String args[]){
-	Scanner sc= new Scanner(System.in);
-	System.out.println("enter the size");
-	int size;
-	size= sc.nextInt();
-	int arr[]=new int[size];
-	int j,temp=0;
-	System.out.println("enter the elements");
-	for(j=0;j<size;j++){
-		arr[j]=sc.nextInt();
-	}
-	int ele;
-	System.out.println("enter the element u wanna to search");
-	ele= sc.nextInt();
-	for(j=0;j<size;j++){
-		if(arr[j]==ele){
-		temp=1;
-		}
-	}
-	if(temp==1)
-		System.out.println(ele+" element found");
-	else
-		System.out.println(ele+" element not found");
-	}
+public class p13 {
 
+ public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the size of the array:");
+     int size;
+     size=sc.nextInt();
+     int arr[ ]=new int[size];
+  int j;
+  System.out.println("Enter the Element of the array:");
+        for(j=0;j<size;j++)
+         {
+         arr[j]=sc.nextInt(); 
+         }
+        System.out.println("After reversing array is :");
+         for(int i=size-1;i>=0;i--)
+        {
+         System.out.print(arr[i]+" ");
+         }
+   
+ }
 }
