9cbe4249f5c3aaabd5471fdb2611041ab99fa7b5
vkchiddenfeeling1998 <67635161+vkchiddenfeeling1998@users.noreply.github.com>
2020-09-19 00:46:32 +0530

    Update Q24search.java

--- Assignment1/Q24search.java
+++ Assignment1/Q24search.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
-public class Q28stringarray{
-	public static void main(String args[]){
-	String[] strArray = {"Ravi","Shruti","Tina","pina"};
+public class Q24search {
 
-	for (int i = 0; i < strArray.length; i++) {
-	System.out.print(strArray[i]);
-}
+ public static void main(String[] args) {
+  Scanner sc=new Scanner(System.in);
+  System.out.println("Enter the size of the array:");
+  int i,temp=0,size;
+     size=sc.nextInt();
+     int arr[ ]=new int[size];
+  System.out.println("Enter the Element of the array:");
+      for(i=0;i<size;i++)
+     {
+         arr[i]=sc.nextInt();
+     }
+      int ele;
+      System.out.print("\nEnter the search element:");
+      ele=sc.nextInt();
+
+      System.out.print("\nArray elements are:");
+      for(i=0;i<size;i++)
+      {
+       System.out.print(arr[i]+" ");
+      }
+      for(i=0;i<size;i++)
+      {
+          if(arr[i]==ele)
+              temp=1;
+      }
+      if(temp==1)
+       System.out.print("\nElement found....");
+      else
+       System.out.print("\nElement not found....");
+
+
+ }
 }
